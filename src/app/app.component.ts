617ceca6c7b7b69a9e3077ec7daf8c4fa5cec82a
João Paulo <jpjoao1001@gmail.com>
2021-04-03 17:04:40 -0300

    added a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewComponentComponent } from './new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  constructor(private NewComponent: NewComponentComponent) {}
+
+  async renderNewComponent(): Promise<any> {
+    return await NewComponentComponent
+  }
 }
